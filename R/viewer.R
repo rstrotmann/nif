15533e4be0e6742133b93425c643f69902394065
rstrotmann <rstrotmann@users.noreply.github.com>
2023-07-13 05:49:32 +0200

    add dose filter to nif_viewer

--- R/viewer.R
+++ R/viewer.R
@@ -28,10 +28,12 @@ nif_viewer <- function(nif) {
   nif_viewer.ui <- shiny::fluidPage(
     title="NIF viewer",
     shinyjs::useShinyjs(),
+
     shiny::fluidRow(
       style="padding:10px; spacing:10",
       shiny::column(3,
-        shiny::selectInput("subject", label="subject", choices=sbs),
+        shiny::selectInput("subject", label="subject", choices=sbs,
+                           selected=sbs[1]),
         shiny::actionButton("prev.sb", "previous"),
         shiny::actionButton("next.sb", "next")
       ),
@@ -50,8 +52,10 @@ nif_viewer <- function(nif) {
 
       shiny::column(2, checkboxInput("log_yscale", "log y-scale")),
       shiny::column(3,
-        shiny::selectInput("dose", label="dose", choices=c("all", doses)))
+        shiny::selectInput("dose", label="dose filter",
+                           choices=c("all", doses), selected="all"))
     ),
+
     hr(),
     shiny::plotOutput("plot.pc"),
     shiny::plotOutput("plot.dose"),
@@ -61,12 +65,12 @@ nif_viewer <- function(nif) {
     )
   )
 
-  current_nif <- nif
-  current_sbs <- current_nif %>%
-    distinct(USUBJID) %>%
-    pull(USUBJID)
+
 
   nif_viewer.server <- function(input, output, session) {
+    current_nif <- reactiveVal(nif)
+    current_sbs <- reactiveVal(sbs)
+
     max.time <- function() {
       if(input$timeselect=="indiv") {
         return(nif %>%
@@ -82,37 +86,30 @@ nif_viewer <- function(nif) {
       }
     }
 
-    # current_nif <- nif
-    # current_sbs <- current_nif %>%
-    #   distinct(USUBJID) %>%
-    #   pull(USUBJID)
-
     output$plot.pc <- shiny::renderPlot({
       y_scale_type <- ifelse(input$log_yscale, "log", "lin")
       suppressWarnings(print(
-        nif::nif_plot_id(current_nif, input$subject, max.time=max.time(), y.scale=y_scale_type)))
+        nif::nif_plot_id(current_nif(), input$subject, max.time=max.time(), y.scale=y_scale_type)))
     }, height=350)
 
     output$plot.dose <- shiny::renderPlot({
       suppressWarnings(print(
-        nif::dose_plot_id(current_nif, input$subject, max.dose=max.dose, max.time=max.time())))
+        nif::dose_plot_id(current_nif(), input$subject, max.dose=max.dose, max.time=max.time())))
     }, height=250)
 
     shiny::observeEvent(input$prev.sb, {
-      current <- which(current_sbs==input$subject)
+      current <- which(current_sbs()==input$subject)
       if(current > 0) {
-        # shiny::updateSelectInput(session, "subject", choices=sbs, selected=sbs[current-1])
         shiny::updateSelectInput(session, "subject",
-                                 choices=current_sbs, selected=current_sbs[current-1])
+                                 choices=current_sbs(), selected=current_sbs()[current-1])
       }
     })
 
     shiny::observeEvent(input$next.sb, {
-      current <- which(current_sbs==input$subject)
-      if(current < length(current_sbs)) {
-        # shiny::updateSelectInput(session, "subject", choices=sbs, selected=sbs[current+1])
+      current <- which(current_sbs()==input$subject)
+      if(current < length(current_sbs())) {
         shiny::updateSelectInput(session, "subject",
-                                 choices=current_sbs, selected=current_sbs[current+1])
+                                 choices=current_sbs(), selected=current_sbs()[current+1])
       }
     })
 
@@ -126,15 +123,19 @@ nif_viewer <- function(nif) {
 
     shiny::observeEvent(input$dose, {
       if(input$dose != "all"){
-        current_nif <- nif %>%
-          filter(AMT==as.numeric(input$dose))
+        current_nif(
+          nif %>%
+            filter(DOSE==as.numeric(input$dose)))
       } else {
-        current_nif <- nif
+        current_nif(nif)
       }
-      current_sbs <- current_nif %>%
+
+      current_sbs(current_nif() %>%
         distinct(USUBJID) %>%
-        pull(USUBJID)
-      updateSelectInput(session, "subject", choices=current_sbs, selected=1)
+        pull(USUBJID))
+
+      updateSelectInput(
+        session, "subject", choices=current_sbs(), selected=current_sbs()[1])
     })
   }
 
