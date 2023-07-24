8574eaacecc98e8da4326ffcacf7fb192621d1ff
rstrotmann <rstrotmann@users.noreply.github.com>
2023-07-24 08:30:07 +0200

    add vignette

---
+++ doc/nif-vignette.R
@@ -0,0 +1,48 @@
+## ----echo=F, message=F, warning=F---------------------------------------------
+library(tidyverse)
+library(knitr)
+
+## ---- eval=FALSE, echo=T------------------------------------------------------
+#  nif::read_sdtm_sas("path/to/sdtm/data")
+
+## ---- eval=FALSE, echo=T------------------------------------------------------
+#  read_sdtm_sas("path/to/sdtm/data", "dm", "vs", "ex", "pc", "lb", "ae")
+
+## -----------------------------------------------------------------------------
+sdtm.expl <- nif::sdtm(nif::examplinib)
+sdtm.expl
+
+## -----------------------------------------------------------------------------
+nif::suggest(sdtm.expl)
+
+## -----------------------------------------------------------------------------
+sdtm.expl <- nif::sdtm(nif::examplinib) %>% 
+  nif::add_mapping("EXAMPLINIB", "RS2023")
+
+## -----------------------------------------------------------------------------
+nif.expl <- nif::make_nif(sdtm.expl)
+
+## -----------------------------------------------------------------------------
+nif.expl %>% 
+  as.data.frame() %>% 
+  distinct(EPOCH, ACTARMCD, ACTARM)
+
+## -----------------------------------------------------------------------------
+nif.expl <- nif::make_nif(sdtm.expl, silent=T) %>%
+  mutate(PERIOD=str_sub(EPOCH, -1, -1)) %>% 
+  mutate(TREATMENT=str_sub(ACTARMCD, PERIOD, PERIOD)) %>% 
+  mutate(FASTED=case_when(TREATMENT=="A" ~ 1, .default=0))
+
+## -----------------------------------------------------------------------------
+nif.expl %>%
+  dplyr::select(USUBJID, PERIOD, TREATMENT, FASTED, TIME, EVID, AMT, DV) %>% 
+  as.data.frame() %>% 
+  head() %>% 
+  kable()
+
+## ---- warning=F, fig.width=4, fig.height=3------------------------------------
+plot(nif.expl)
+
+## ---- warning=F, fig.width=4, fig.height=3------------------------------------
+plot(nif.expl, analyte="RS2023", group="FASTED", max_x=24, mean=T, points=T)
+
