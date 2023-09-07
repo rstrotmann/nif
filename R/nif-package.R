f70864ed830dac1883024bbf640eac0cbd42acde
rstrotmann <rstrotmann@users.noreply.github.com>
2023-09-07 16:05:13 +0200

    fix global variable issue for dplyr fields

---
+++ R/nif-package.R
@@ -0,0 +1,24 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+utils::globalVariables(c(
+  "labdate", "LBDTC", "LBSPEC", "LBTESTCD", "USUBJID", "FIRSTDTC", "DTC",
+  "LBSTRESN", "LBDY", "STUDYID", "ANALYTE", "CMT", "EVID", "TIME", "NTIME",
+  "DV", "AMT", "ID", "DOSE", "AGE", "SEX", "RACE", "ACTARMCD", "HEIGHT",
+  "WEIGHT", "BMI", "PCDTC", "dtc", "PCRFTDTC", "ref.dtc", "dtc.date",
+  "ref.date", "PCTESTCD", "ref.time", "time", "EXTRT", "admin.time", "REF",
+  "EXENDTC", "EXSEQ", "EXSTDTC", "start", "end", "end.time", "start.time",
+  "EXDOSE", "EXSTDY", "EXENDY", "EPOCH", "start.date", "end.date", "SUBJID",
+  "runif", "ACTARM", "VSTESTCD", "VSSTRESN", "ut", "RFICDTC", "BRTHDTC",
+  "brthyr", "refdtc", "ETHNIC", "COUNTRY", "ARM", "PCSPEC", "PCSTAT", "PCELTM",
+  "PCSTRESN", "PERIOD", "RFSTDTC", "c_centr", "c_metab", "EXDY", "rnorm",
+  "VSORRES", "VSORRESU", "centr", "v_centr", "centr.err", "peri", "v_peri",
+  "metab", "v_metab", "t.ke", "eta.ke", "t.ka", "eta.ka", "FOOD", "t.ka1",
+  "t.d1", "eta.d1", "t.fm", "eta.fm", "t.cl", "eta.cl", "t.kem", "eta.kem",
+  "eta.fpar", "t.fpar1", "t.q", "eta.q", "/<-", "dt", "dur<-", "renal",
+  "metab_excr", "sd", "GROUP", "first_admin_time", "regimen", "PCTEST",
+  "no.analyte", "bl_wt", "rc", "maxwt"))
