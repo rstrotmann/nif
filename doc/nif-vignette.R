34a2391b5d93caede852cedc0d646dd10924273e
rstrotmann <rstrotmann@users.noreply.github.com>
2023-09-17 10:36:12 +0200

    update vignettes

--- doc/nif-vignette.R
+++ doc/nif-vignette.R
@@ -18,46 +18,53 @@ library(nif)
 #  read_sdtm_sas("path/to/sdtm/data", "dm", "vs", "ex", "pc", "lb", "ae")
 
 ## -----------------------------------------------------------------------------
-sdtm.expl <- examplinib
-sdtm.expl
+examplinib$domains[["dm"]] %>% head()
 
 ## -----------------------------------------------------------------------------
-suggest(sdtm.expl)
+dm <- examplinib$dm
 
 ## -----------------------------------------------------------------------------
-sdtm.expl <- examplinib %>% 
-  add_analyte_mapping("EXAMPLINIB", "RS2023")
+examplinib
 
 ## -----------------------------------------------------------------------------
-nif.expl <- make_nif(sdtm.expl)
+suggest(examplinib)
 
 ## -----------------------------------------------------------------------------
-nif.expl %>% 
+sdtm_expl <- examplinib %>% 
+  add_analyte_mapping("EXAMPLINIB", "RS2023") %>% 
+  add_metabolite_mapping("RS2023", "RS2023487A")
+
+## -----------------------------------------------------------------------------
+nif_expl <- make_nif(sdtm_expl)
+
+## -----------------------------------------------------------------------------
+nif_expl %>% 
   as.data.frame() %>% 
   distinct(EPOCH, ACTARMCD, ACTARM)
 
 ## -----------------------------------------------------------------------------
-nif.expl <- make_nif(sdtm.expl, silent=T) %>%
+nif_expl <- make_nif(sdtm_expl, silent=T) %>%
   mutate(PERIOD=str_sub(EPOCH, -1, -1)) %>% 
   mutate(TREATMENT=str_sub(ACTARMCD, PERIOD, PERIOD)) %>% 
   mutate(FASTED=case_when(TREATMENT=="A" ~ 1, .default=0))
 
 ## -----------------------------------------------------------------------------
-nif.expl %>%
-  select(USUBJID, PERIOD, TREATMENT, FASTED, TIME, EVID, AMT, DV) %>% 
+nif_expl %>%
+  select(USUBJID, PERIOD, TREATMENT, FASTED, TIME, EVID, AMT, ANALYTE, DV) %>% 
+  arrange(USUBJID, PERIOD, TREATMENT, FASTED, TIME, EVID, ANALYTE) %>% 
   as.data.frame() %>% 
   head() %>% 
   kable()
 
 ## ---- warning=F, fig.width=4, fig.height=3------------------------------------
-plot(nif.expl)
+plot(nif_expl)
 
 ## ---- warning=F, fig.width=4, fig.height=3------------------------------------
-plot(nif.expl, analyte="RS2023", group="FASTED", max_x=24, mean=F, points=T, nominal_time=T)
+plot(nif_expl, analyte="RS2023", group="FASTED", max_x=24, mean=F, points=T, nominal_time=T)
 
 ## ---- warning=F, fig.width=4, fig.height=3------------------------------------
-plot(nif.expl, analyte="RS2023", group="FASTED", max_x=24, mean=T)
+plot(nif_expl, analyte="RS2023", group="FASTED", max_x=24, mean=T)
 
 ## ---- eval=FALSE, echo=T------------------------------------------------------
-#  nif_viewer(nif.expl)
+#  nif_viewer(nif_expl)
 
