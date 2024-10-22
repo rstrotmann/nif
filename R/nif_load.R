7ab8261b8b98a1cb8d1020b697ba8405474107b7
rstrotmann <rstrotmann@users.noreply.github.com>
2024-10-22 11:13:58 +0200

    add first draft of read_nif()

---
+++ R/nif_load.R
@@ -0,0 +1,14 @@
+read_nif <- function(filename, format="csv") {
+
+  # if(format == "csv"){
+    raw <- read.csv(
+      filename,
+      sep=",",
+      numerals=c("no.loss"))
+    missing_fields <- minimal_nif_fields[!minimal_nif_fields %in% names(raw)]
+    if(length(missing_fields) != 0)
+      stop("Required fields missing!")
+  # }
+  out <- new_nif(raw)
+  return(out)
+}
