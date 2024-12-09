a301d8407896205db665840264334a03cb12a5c1
rstrotmann <rstrotmann@users.noreply.github.com>
2024-12-09 10:27:41 +0100

    remove plot_old

--- tests/testthat/test-nif_exploration.R
+++ tests/testthat/test-nif_exploration.R
@@ -3,6 +3,7 @@ test_that("nif summary works", {
   temp <- examplinib_sad_nif %>%
     summary()
   expect_s3_class(temp, "summary_nif")
+  expect_output(print(summary(examplinib_sad_nif)))
 })
 
 
@@ -44,15 +45,71 @@ test_that("dose_plot_id works", {
 })
 
 
+test_that("covariate_hist works", {
+  expect_no_error(examplinib_poc_nif %>%
+    covariate_hist("AGE"))
+  expect_no_error(examplinib_poc_nif %>%
+                    covariate_hist("AGE", group = "SEX"))
+})
 
 
+test_that("covariate_barplot works", {
+  expect_no_error(examplinib_poc_nif %>%
+                    covariate_barplot("RACE"))
+  expect_no_error(examplinib_poc_nif %>%
+                    covariate_barplot("RACE", group = "SEX"))
+})
 
 
+test_that("x_by_y works", {
+  expect_no_error(wt_by_sex(examplinib_poc_nif))
+  expect_no_error(wt_by_race(examplinib_poc_nif))
+  expect_no_error(wt_by_ht(examplinib_poc_nif))
+  expect_no_error(ht_by_wt(examplinib_poc_nif))
+  expect_no_error(bmi_by_age(examplinib_poc_nif))
+})
 
 
+test_that("time_by_ntime works", {
+  expect_no_error(time_by_ntime(examplinib_sad_nif))
+})
 
 
+test_that("administration_summary works", {
+  expect_equal(
+    nrow(administration_summary(examplinib_poc_nif)),
+    1
+  )
+})
 
+
+test_that("mean_dose_plot works", {
+  expect_no_error(
+    mean_dose_plot(examplinib_poc_nif)
+  )
+})
+
+
+test_that("subs_per_dose_level works", {
+  expect_equal(
+    subs_per_dose_level(examplinib_sad_nif)$N,
+    c(3L, 3L, 3L, 3L, 6L, 3L, 18L, 6L, 3L)
+  )
+})
+
+
+test_that("obs_per_dose_level works", {
+  expect_equal(
+    obs_per_dose_level(examplinib_sad_nif)$N,
+    c(51L, 51L, 51L, 51L, 102L, 51L, 306L, 102L, 51L)
+  )
+})
+
+
+# test_that("plot_old works", {
+#   expect_no_error(p <- plot_old(examplinib_sad_nif, "RS2023"))
+#   expect_no_error(p <- plot_old(examplinib_sad_nif, "RS2023", mean = TRUE))
+# })
 
 
 
