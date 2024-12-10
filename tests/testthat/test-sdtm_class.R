60078e95e114e9f1dffb94dee3d6bbcbf4cd0d0b
rstrotmann <rstrotmann@users.noreply.github.com>
2024-12-10 09:25:05 +0100

    add tests

--- tests/testthat/test-sdtm_class.R
+++ tests/testthat/test-sdtm_class.R
@@ -36,6 +36,39 @@ test_that("new_sdtm", {
 test_that("sdtm summary", {
   expect_no_error(summary(examplinib_sad))
   expect_output(print(summary(examplinib_sad)))
+  test <- examplinib_fe
+  test$analyte_mapping <- NULL
+  expect_no_error(summary(test))
+})
+
+
+test_that("sdtm_summary works with metabolite mapping", {
+  test <- examplinib_fe %>%
+    add_time_mapping(
+     "PREDOSE" = 0,
+     "HOUR 0.5" = 0.5,
+     "HOUR 1" = 1,
+     "HOUR 1.5" = 1.5,
+     "HOUR 2" = 2,
+     "HOUR 3" = 3,
+     "HOUR 4" = 4,
+     "HOUR 6" = 6,
+     "HOUR 8" = 8,
+     "HOUR 10" = 10,
+     "HOUR 12" = 12,
+     "HOUR 24" = 24,
+     "HOUR 48" = 48,
+     "HOUR 72" = 72,
+     "HOUR 96" = 96,
+     "HOUR 144" = 144,
+     "HOUR 168" = 168
+    )
+  expect_no_error(summary(test))
+})
+
+
+test_that("suggest works", {
+  expect_message(suggest(examplinib_poc, consider_nif_auto = TRUE))
 })
 
 
--- tests/testthat/test-sdtm_exploration.R
+++ tests/testthat/test-sdtm_exploration.R
@@ -12,3 +12,58 @@ test_that("filter_correct_date_format works", {
     expect_no_error(filter_correct_date_format(test))
   )
 })
+
+
+test_that("check_date_format, check_date_time_format works", {
+  expect_no_message(check_date_format(domain(examplinib_sad, "pc")))
+  expect_no_message(check_date_time_format(domain(examplinib_sad, "pc")))
+  expect_no_message(check_missing_time(domain(examplinib_sad, "pc")))
+})
+
+
+test_that("check_last_exendtc works", {
+  check_last_exendtc(domain(examplinib_poc, "ex"))
+})
+
+
+test_that("check_sdtm works", {
+  expect_no_message(check_sdtm(examplinib_poc))
+})
+
+
+test_that("plot.sdtm works", {
+  expect_no_error(plot(examplinib_poc, domain = "dm"))
+  expect_no_error(plot(examplinib_poc, domain = "ex"))
+  expect_no_error(plot(examplinib_poc, domain = "pc"))
+  expect_no_error(plot(examplinib_poc, domain = "lb"))
+  expect_no_error(plot(examplinib_poc, domain = "vs"))
+
+  temp <- examplinib_poc
+  xx <- domain(temp, "pc") %>%
+    mutate(DOMAIN = "XX") %>%
+    mutate(XXDTC = PCDTC)
+  temp$domains$xx <- xx
+  expect_no_error(plot(temp, domain = "xx"))
+})
+
+
+test_that("disposition_summary works", {
+  expect_equal(
+    disposition_summary(examplinib_sad),
+    as.data.frame(tribble(
+      ~ACTARMCD, ~ONGOING,  ~N,
+      "C1",    FALSE,  3L,
+      "C10",    FALSE, 12L,
+      "C2",    FALSE,  3L,
+      "C3",    FALSE,  3L,
+      "C4",    FALSE,  3L,
+      "C5",    FALSE,  6L,
+      "C6",    FALSE,  3L,
+      "C7",    FALSE,  6L,
+      "C8",    FALSE,  6L,
+      "C9",    FALSE,  3L,
+      "SCRNFAIL",       NA, 16L
+    ))
+  )
+
+})
