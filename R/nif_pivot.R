03f4e49c35e112e496af25adade7a847a52b6c93
rstrotmann <rstrotmann@users.noreply.github.com>
2025-09-11 07:48:05 +0200

    add add_time_deviation()

---
+++ R/nif_pivot.R
@@ -0,0 +1,62 @@
+#' Calculate time deviations for observations
+#'
+#' Calculate the time difference in hours between the actual time after dose
+#' (TAD) and the scheduled time (NTIME) of observations.
+#'
+#' If NTIME == 0, the data point is considered pre-dose, and instead of TAD, the
+#' (negative) time difference to the next administration is calculated!
+#'
+#' @param obj A nif object.
+#'
+#' @returns A nif object with the 'time_diff' field added
+#' @export
+add_time_deviation <- function(obj) {
+  # input validation
+  validate_nif(obj)
+
+  # ensure that TAD and NTIME fields are present and not NA
+  required_fields <- c("TAD", "NTIME")
+  missing_fields <- setdiff(required_fields, names(obj))
+  if(length(missing_fields) > 0) {
+    stop(paste0(
+      "Missing ", plural("field", length(missing_fields) > 1),
+      " in nif object: ", nice_enumeration(missing_fields)
+    ))
+  }
+
+  # identify observations without NTIME
+  temp <- obj %>%
+    as.data.frame() %>%
+    filter(EVID == 0) %>%
+    reframe(n_obs = n(), n_missing_ntime = sum(is.na(NTIME)), .by = "ANALYTE")
+
+  # calculate time deviation
+  out <- obj %>%
+    arrange(USUBJID, DTC, -EVID) %>%
+    group_by(PARENT) %>%
+
+    # identify DTC of next administration
+    mutate(next_admin = case_when(EVID == 1 ~ DTC)) %>%
+    fill(next_admin, .direction = "up") %>%
+
+    # calculate time to next dose
+    mutate(TTND = as.numeric(DTC - next_admin, units = "hours")) %>%
+
+    ungroup() %>%
+
+    # calculate time difference
+    mutate(time_diff = round(
+      case_when(
+        NTIME == 0 ~ .data$TTND,
+        .default = .data$TAD - .data$NTIME),
+      3))
+
+    # as.data.frame() %>%
+    # select(USUBJID, EVID, PARENT, ANALYTE, NTIME, TIME, TAD, time_diff)
+
+  return(out)
+}
+
+
+
+
