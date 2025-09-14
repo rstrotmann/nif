0a041f3377f6105f861589a313439013c1960e55
rstrotmann <rstrotmann@users.noreply.github.com>
2025-09-14 09:55:43 +0200

    add stub for pivot_analytes

---
+++ R/nif_pivot.R
@@ -0,0 +1,119 @@
+#' Convert nif object to wide data frame by NTIME
+#'
+#' @param obj A nif object.
+#' @param analyte Analytes to include, defaults to all, if NULL.
+#' @param duplicates
+#' @param duplicate_function
+#' @param silent
+#'
+#' @returns A data frame.
+#' @export
+#'
+#' @examples
+pivot_analytes <- function(
+    obj, analyte = NULL,
+    duplicates = "stop",
+    duplicate_function = mean,
+    silent = NULL) {
+  # input validation
+  validate_nif(obj)
+  validate_char_param(
+    analyte, "analyte", allow_null = TRUE, allow_multiple = TRUE)
+  validate_logical_param(silent, "silent", allow_null = TRUE)
+
+  valid_duplicate_values <- c("stop", "identify", "resolve")
+  if(!duplicates %in% valid_duplicate_values)
+    stop(paste0(
+      "Invalid value for 'duplicates' - must be one of ",
+      nice_enumeration(valid_duplicate_values, conjunction = "or")))
+
+  # ensure TRTDY
+  obj <- obj %>%
+    add_trtdy()
+
+  # prepare observation data
+  if(is.null(analyte)) {
+    analyte = analytes(obj)
+  }
+  obs <- obj %>%
+    as.data.frame() %>%
+    filter(EVID == 0) %>%
+    filter(ANALYTE %in% analyte)
+
+  # ensure NTIME
+  if(!"NTIME" %in% names(obj)) {
+    stop("'NTIME' not found in nif object!")
+  }
+  na_overview <- obs %>%
+    reframe(n_obs = n(), n_NA = sum(is.na(NTIME)), .by = c(ANALYTE))
+  temp <- filter(na_overview, n_obs == n_NA)
+  if(nrow(temp) > 0) {
+    message(
+      "NTIME is all NA for ", plural("analyte", nrow(temp) > 1), " ",
+      nice_enumeration(temp$ANALYTE), "!")
+  }
+  if(sum(na_overview$n_NA) != 0) {
+    conditional_message(
+      "Some observations with NTIME of NA were excluded:\n",
+      df_to_string(na_overview, indent = 2),
+      silent = silent
+    )
+  }
+  obs <- filter(obs, !is.na(NTIME))
+
+  # duplicate handling
+  duplicates <- find_duplicates(
+    obs, c("ID", "ANALYTE", "NTIME", "TRTDY"))
+
+  duplicate_overview <- duplicates %>%
+    reframe(n = n(), .by = "ANALYTE")
+
+  if(nrow(duplicate_overview) > 0) {
+    if(duplicates == "stop") {
+      stop(paste0(
+        "Multiplicate observations found with respect to NTIME and TRTDY:\n",
+        df_to_string(duplicate_overview, indent = 2), "\n\n",
+        "Identify duplicates using the `duplicates = 'identify'` parameter, ",
+        "or have duplicates automatically resolved with `duplicates = 'resolve'` ",
+        "where the resolution function is specified by the `duplicate_function` ",
+        "parameter (default is `mean`)."
+      ))
+    }
+
+    if(duplicates == "identify") {
+      message(paste0(
+        "Multiplicate observations found with respect to NTIME and TRTDY:\n",
+        df_to_string(duplicate_overview, indent = 2), "\n\n",
+        "Only duplicate observations returned!"))
+
+      return(duplicates)
+    }
+
+    if(duplicates == "resolve") {
+      observation = resolve_duplicates(
+        obs,
+        fields = c("ID", "ANALYTE", "NTIME", "TRTDY"),
+        duplicate_function = duplicate_function,
+        na.rm = TRUE)
+      conditional_message(
+        "In observations for ", testcd, " (analyte '", analyte, "'), ",
+        n_dupl, " duplicates were resolved!",
+        silent = silent
+      )
+    }
+  }
+
+
+
+
+
+
+
+
+  # convert to wide table
+  out <- obs %>%
+    select(ID, NTIME, ANALYTE, DV, TRTDY) %>%
+    pivot_wider(names_from = ANALYTE, values_from = DV)
+
+
+}
