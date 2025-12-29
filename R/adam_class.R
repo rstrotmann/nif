2c31cbac4794c5d2bb37e78ef8e309f58c0c8492
rstrotmann <rstrotmann@users.noreply.github.com>
2025-12-29 12:21:38 +0100

    add adam loading and class functions

---
+++ R/adam_class.R
@@ -0,0 +1,155 @@
+#' adam object class constructor
+#'
+#' @param adam_data The ADaM datasets as data frames.
+#'
+#' @returns An adam object.
+#' @export
+adam <- function(adam_data) {
+  temp <- as.list(adam_data)
+  class(temp) <- c("adam", "list")
+  temp
+}
+
+
+#' Validate adam object
+#'
+#' @param obj An adam object.
+#'
+#' @returns Nothing.
+#' @noRd
+validate_adam <- function(obj) {
+  if (!inherits(obj, "adam")) {
+    stop("Input must be a adam object")
+  }
+}
+
+
+#' Summary method for adam objects
+#'
+#' @param object An adam object.
+#' @param ... Further parameters.
+#'
+#' @returns A summary_adam object.
+#' @export
+summary.adam <- function(object, ...) {
+  out <- list(
+    study = character(0),
+    subjects = character(0),
+    adam = object,
+    n_observations = NULL
+    # treatments = character(0),
+    # arms = data.frame(ACTARMCD = character(0), ACTARM = character(0)),
+    # doses = data.frame(EXTRT = character(0), EXDOSE = numeric(0)),
+    # specimens = character(0),
+    # analytes = data.frame(PCTEST = character(0), PCTESTCD = character(0)),
+    # analyte_mapping = object$analyte_mapping,
+    # metabolite_mapping = object$metabolite_mapping,
+    # time_mapping = object$time_mapping,
+    # hash = rlang::hash(object),
+    # last = last_dtc(object),
+    # title = trial_title(object),
+    # dco = trial_dco(object)
+  )
+
+  out$study <- purrr::map(
+    object,
+    function(x) {
+      if ("STUDYID" %in% names(x)) {
+        unique(x$STUDYID)
+      } else {
+        NULL
+      }
+    }
+  ) |>
+    unlist() |>
+    as.character() |>
+    unique()
+
+  out$subjects <- purrr::map(
+    object,
+    function(x) {
+      if ("USUBJID" %in% names(x)) {
+        unique(x$USUBJID)
+      } else {
+        NULL
+      }
+    }
+  ) |>
+    unlist() |>
+    as.character() |>
+    unique()
+
+  out$n_observations <- data.frame(
+    DATASET = names(object),
+    OBSERVATIONS = as.numeric(lapply(object, nrow))
+  )
+
+  out$datasets <- names(object)
+
+  class(out) <- "summary_adam"
+  out
+}
+
+
+#' Print function for summary_adam objects
+#'
+#' @param x The summary_adam object.
+#' @param ... Further parameters.
+#'
+#' @returns Nothing.
+#' @export
+#' @noRd
+print.summary_adam <- function(x, ...) {
+  indent <- 2
+  hline <- paste0(rep("-", 8), collapse = "")
+  spacer <- paste0(rep(" ", indent), collapse = "")
+
+  cat(paste(hline, "ADaM data set summary", hline, "\n"))
+
+  cat(paste0(
+    "Data from ", length(x$subjects), " subjects across ",
+    length(x$study), " ",
+    plural("study", length(x$study) > 1), ":\n",
+    nice_enumeration(x$study),
+    "\n\n"
+  ))
+
+  cat(paste0(
+    "Data disposition:\n",
+    df_to_string(x$n_observations, indent = 2)
+  ))
+}
+
+
+#' Print function for adam objects.
+#'
+#' @param x The adam object.
+#' @param ... Further parameters.
+#'
+#' @returns Nothing.
+#' @export
+#' @noRd
+print.adam <- function(x, ...) {
+  print(summary(x))
+}
+
+
+#' Retrieve dataset from adam object
+#'
+#' @param adam An adam object.
+#' @param name The dataset name as character.
+#'
+#' @returns A data frame.
+#' @export
+dataset <- function(adam, name) {
+  # input validation
+  validate_adam(adam)
+  validate_char_param(name, "name")
+
+  if (!name %in% names(adam))
+    stop(paste0(
+      "Dataset ", name, " not found in adam object!"
+    ))
+
+  adam[[name]]
+}
---
+++ R/adam_load.R
@@ -0,0 +1,101 @@
+read_adam <- function(
+    data_path,
+    dataset = NULL,
+    format = "sas", delim = ",", ...) {
+  # validate input
+  validate_char_param(data_path, "data_path")
+  validate_char_param(dataset, "dataset", allow_null = TRUE,
+                      allow_multiple = TRUE)
+  validate_char_param(format, "format")
+
+  # Validate data_path
+  if (!dir.exists(data_path)) {
+    stop("data_path does not exist: ", data_path, call. = FALSE)
+  }
+
+  # Validate format
+  valid_formats <- c("sas", "xpt", "csv")
+  if (!format %in% valid_formats) {
+    stop("format must be one of: ", paste(valid_formats, collapse = ", "),
+         call. = FALSE)
+  }
+
+  # Get file extension based on format
+  file_ext <- switch(format,
+                     "sas" = ".sas7bdat",
+                     "xpt" = ".xpt",
+                     "csv" = ".csv",
+                     "xlsx" = ".xlsx"
+  )
+
+  # set domains
+  if (is.null(dataset)) {
+    temp <- list.files(file.path(data_path), pattern = paste0(".*\\", file_ext))
+    dataset <- gsub(paste0("^(.*)\\", file_ext), "\\1", temp)
+    # dataset names starting with an underscore are omitted
+    dataset <- dataset[substring(dataset, 1, 1) != "_"]
+  }
+
+  # Check if all required files exist
+  missing_files <- character()
+  for (x in dataset) {
+    file_path <- file.path(data_path, paste0(x, file_ext))
+    if (!file.exists(file_path)) {
+      missing_files <- c(missing_files, file_path)
+    }
+  }
+
+  if (length(missing_files) > 0) {
+    stop("The following files do not exist:\n",
+         paste(missing_files, collapse = "\n"),
+         call. = FALSE
+    )
+  }
+
+  out <- list()
+
+  if (format == "sas") {
+    for (x in dataset) {
+      out[[tolower(x)]] <- as.data.frame(haven::read_sas(
+        file.path(data_path, paste0(x, ".sas7bdat"))
+      ))
+    }
+  }
+
+  if (format == "xpt") {
+    for (x in dataset) {
+      out[[x]] <- as.data.frame(haven::read_xpt(
+        file.path(data_path, paste0(x, ".xpt"))
+      ))
+    }
+  }
+
+  if (format == "csv") {
+    for (x in dataset) {
+      out[[x]] <- as.data.frame(
+        # readr::read_delim(
+        readr::read_delim(
+          file.path(data_path, paste0(x, ".csv")),
+          delim = delim,
+          show_col_types = FALSE
+        )
+      )
+    }
+  }
+
+  # if (format == "xlsx") {
+  #   for (x in domain) {
+  #     out[[x]] <- as.data.frame(
+  #       readxl::read_xlsx(
+  #         file.path(data_path, paste0(x, ".xlsx")),
+  #         ...
+  #       )
+  #     )
+  #   }
+  # }
+
+  if (length(out) == 0) {
+    stop("no dataset found")
+  }
+  adam(out)
+}
