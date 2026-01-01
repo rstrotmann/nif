d38690e53f08d7409e94cc3ee0f91025c6347f16
rstrotmann <rstrotmann@users.noreply.github.com>
2026-01-01 14:04:50 +0100

    enhance ddt_standard_fields, improve message in ddt()

--- R/nif_ddt.R
+++ R/nif_ddt.R
@@ -1,34 +1,45 @@
 ddt_standard_fields <- tibble::tribble(
-  ~name, ~definition, ~type, ~description, ~unit, ~source,
-  "REF", "consecutive record number", "integer", "Unique number for each row", NA, "produced",
-  "STUDYID", "Study", "character", "Study identification number", NA, "DM: STUDYID",
-  "ID", "Subject identifier", "numeric", "Unique subject ID across all studies", NA, "produced",
-  "USUBJID", "USUBJID in source", "character", "Unique subject ID in study", NA, "DM: USUBJID",
-  "AGE", "Age", "numeric", "Age of subjec at study start", "years", "DM: AGE or derived from DM: BRTHDTC",
-  "SEX", "Sex", "0, 1", "0 = Male, 1 = Female", NA, "derived from DM: SEX",
-  "RACE", "Race", "character", "Race category", NA, "DM: RACE",
-  "HEIGHT", "Body height", "numeric", "Baseline body height", "cm", "VS: VSTESTCD = HEIGHT",
-  "WEIGHT", "Body weight", "numeric", "Baseline body weight", "kg", "VS: VSTESTCD = WEIGHT",
-  "BMI", "Body Mass Index", "numeric", "Baseline BMI", "kg/m^2", "HEIGHT and WEIGHT",
-  "DTC", "datetime", "Datetime", "Date and time of event", "Datetime", "Source SDTM domain",
-  "TIME", "Time since start of treatment", "numeric", "Individual time since individual start of treatment", "hours", "DTC",
-  "NTIME", "Nominal time", "numeric", "Nominal time of event", "hours", "Source SDTM domain",
-  "TAFD", "Time after first dose", "numeric", "Actual time after individual first dose", "hours", "TIME",
-  "TAD", "Time after last dose", "numeric", "Time after individual last dose", "hours", "TIME",
-  "EVID", "Event ID", "0, 1", "0 = Observation, 1 = Administration", NA, "produced",
-  "AMT", "Amount", "numeric", "Dose administered", "mg", "EX: EXDOSE",
-  "ANALYTE", "Analyte", "character", "Assigned name to observation substrate", NA, "EXTRT, xxTESTCD or assigned",
-  "CMT", "Compartment", "numeric", "PK/PD compartment", NA, "produced or assigned",
-  "PARENT", "Parent analyte", "character", "Reference drug name for observations", NA, "automatically or manually assigned",
-  "METABOLITE", "Metabolite", "logical", "Metabolite flag", NA, "automatically or manually assigned",
-  "DOSE", "Dose", "numeric", "Last administerd dose", "mg", "EX: EXDOSE",
-  "DV", "Dependent variable", "numeric", "Dependent variable, NA for administrations", NA, "Source SDTM domain",
-  "MDV", "Missing DV", "numeric", "0 = non-missing DV, 1 = Missing DV", NA, "produced",
-  "IMPUTATION", "Imputation", "character", "time imputation applied to record", NA, "produced",
-  "ACTARMCD", "Actual arm code", "character", "ACTARMCD as in SDTM source", NA, "DM: ACTARMCD"
-)
+         ~name,                                  ~definition,                            ~type,                                              ~description,      ~unit,                               ~source,
+         "REF",                  "Consecutive record number",                        "integer",                              "Unique number for each row",         NA,                            "Produced",
+     "STUDYID",                                      "Study",                      "character",                             "Study identification number",         NA,                         "DM: STUDYID",
+          "ID",                         "Subject identifier",                        "numeric",                    "Unique subject ID across all studies",         NA,                            "Produced",
+     "USUBJID",                          "USUBJID in source",                      "character",                              "Unique subject ID in study",         NA,                         "DM: USUBJID",
+         "AGE",                                        "Age",                        "numeric",                            "Age of subjec at study start",    "years", "DM: AGE or derived from DM: BRTHDTC",
+         "SEX",                                        "Sex",                           "0, 1",                                    "0 = Male, 1 = Female",         NA,                "Derived from DM: SEX",
+        "RACE",                                       "Race",                      "character",                                           "Race category",         NA,                            "DM: RACE",
+      "HEIGHT",                                "Body height",                        "numeric",                                    "Baseline body height",       "cm",               "VS: VSTESTCD = HEIGHT",
+      "WEIGHT",                                "Body weight",                        "numeric",                                    "Baseline body weight",       "kg",               "VS: VSTESTCD = WEIGHT",
+         "BMI",                            "Body Mass Index",                        "numeric",                                            "Baseline BMI",   "kg/m^2",                   "HEIGHT and WEIGHT",
+         "DTC",                                   "Datetime",                       "datetime",                                  "Date and time of event", "Datetime",                         "SDTM domain",
+        "TIME",              "Time since start of treatment",                        "numeric",     "Individual time since individual start of treatment",    "hours",                                 "DTC",
+       "NTIME",                               "Nominal time",                        "numeric",                                   "Nominal time of event",    "hours",                         "SDTM domain",
+        "TAFD",                      "Time after first dose",                        "numeric",                 "Actual time after individual first dose",    "hours",                                "TIME",
+         "TAD",                       "Time after last dose",                        "numeric",                         "Time after individual last dose",    "hours",                                "TIME",
+        "EVID",                                   "Event ID",                           "0, 1",                     "0 = Observation, 1 = Administration",         NA,                            "Produced",
+         "AMT",                                     "Amount",                        "numeric",                                       "Dose administered",       "mg",                          "EX: EXDOSE",
+     "ANALYTE",                                    "Analyte",                      "character",                  "Assigned name to observation substrate",         NA,         "EXTRT, xxTESTCD or assigned",
+         "CMT",                                "Compartment",                        "numeric",                                       "PK/PD compartment",         NA,                "Produced or assigned",
+      "PARENT",                             "Parent analyte",                      "character",                    "Reference drug name for observations",         NA,  "Automatically or manually assigned",
+  "METABOLITE",                                 "Metabolite",                        "logical",                                         "Metabolite flag",         NA,  "Automatically or manually assigned",
+        "DOSE",                                       "Dose",                        "numeric",                                   "Last administerd dose",       "mg",                          "EX: EXDOSE",
+          "DV",                         "Dependent variable",                        "numeric",              "Dependent variable, NA for administrations",         NA,                         "SDTM domain",
+         "MDV",                                 "Missing DV",                        "numeric",                      "0 = non-missing DV, 1 = Missing DV",         NA,                            "Produced",
+  "IMPUTATION",                                 "Imputation",                      "character",                       "time imputation applied to record",         NA,                            "Produced",
+    "ACTARMCD",                            "Actual arm code",                      "character",                              "ACTARMCD as in SDTM source",         NA,                        "DM: ACTARMCD",
+       "TRTDY",                              "Treatment day",                        "numeric",                               "Day after treatment start",         NA,                         "SDTM domain",
+    "BL_CREAT",                        "Baseline creatinine",                        "numeric",                      "Serum creatinine value at baseline",   "umol/l",                                  "LB",
+     "BL_CRCL",             "Baseline creatinnine clearance",                        "numeric", "Creatinine clearance based on baseline serum creatinine",   "ml/min",                              "LB, DM",
+    "BL_RENAL",              "Baseline renal function class", "normal, mild, moderate, severe",   "Renal function category at baseline, based on BL_CRCL",         NA,                "Derived from BL_CRCL",
+        "DVBL",      "Baseline value for dependent variable",                        "numeric",                                    "DV value at baseline",         NA,                         "SDTM domain",
+       "DVCFB", "Change from baseline of dependent variable",                        "numeric",                                   "DV difference to DVBL",         NA,               "Derived from DV, DVBL",
+          "DL",                                 "Dose level",                        "numeric",                                 "Dose at treatment start",       "mg",                   "Derived from DOSE"
+  )
 
-#' Data definition table for NIF object
+#' Data definition table for nif object
+#'
+#' Generate a data definition table from a nif object. Known nif columns are
+#' annotated automatically, for unknown fields, NA values are produced. Further
+#' specifications can be added manually using `add_dd()`.
 #'
 #' @param obj A nif object.
 #' @param silent Suppress messages, defaults to nif_option setting if NULL.
@@ -77,18 +88,24 @@ ddt <- function(obj, silent = NULL) {
   # further fields
   further_name <- setdiff(names(obj), unique(out$name))
   further_type <- as.character(lapply(obj, class)[further_name])
+
   further <- data.frame(name = further_name, type = further_type)
 
   out <- bind_rows(out, further) %>%
     filter(.data$name %in% names(obj)) %>%
     as.data.frame()
 
-  conditional_message(
-    "The following fields in the data definition table could not be ",
-    "automatically generated: ",
-    nice_enumeration(further$name),
-    silent = silent
-  )
+  if (nrow(further) > 0) {
+    conditional_cli({
+      cli_alert_warning("Some data definition fields could not be automatically generated:")
+      cli_text(nice_enumeration(further$name))
+      # cli_text("Use `add_ddt()` to manually add definitions!")
+    },
+    silent = silent)
+  }
+
+
+
   return(out)
 }
 
