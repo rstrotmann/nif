bfa755ed5b3569feeb48afda23946cbc9d5642c8
rstrotmann <rstrotmann@users.noreply.github.com>
2026-01-18 10:23:07 +0100

    add make_bintime and bintime_plot

--- R/nif_qtime.R
+++ R/nif_qtime.R
@@ -1,3 +1,149 @@
+#' Make BINTIME field
+#'
+#' @param method Univariate class intervals method, can be one of jenks, kmeans,
+#' pretty, quantile, hclust, sd, bclust or fisher. See classInt::classInterval()
+#' for details.
+#' @param silent Suppress messages.
+#' @param obj A nif object.
+#'
+#' @returns A nif object with the BINTIME, BIN_LEFT and BIN_RIGHT fields added.
+#' @importFrom classInt classIntervals
+#' @export
+add_bintime <- function(
+    obj,
+    method = "fisher",
+    silent = NULL) {
+  # input validation
+  validate_nif(obj)
+  validate_char_param(method, "method")
+  if(!method %in%c("jenks", "kmeans", "pretty", "quantile", "hclust", "sd",
+                 "bclust", "fisher")) {
+    stop(paste0("Method ", method, " not implemented!"))
+  }
+
+  # calculate bins
+  obj <- ensure_tafd(obj)
+  bins <- classInt::classIntervals(obj$TAFD, style = method)
+  bin_par <- attributes(bins)$parameters
+
+  breaks <- sort(bins$brks)
+  labels <- bin_par[,3]
+
+  bin_par <- as.data.frame(bin_par[order(bin_par[,1]),]) |>
+    arrange(min) |>
+    mutate(left = breaks[1:length(breaks) - 1]) |>
+    mutate(right = breaks[-1]) |>
+    mutate(label = round(.data[["class mean"]]))
+
+  obj |>
+    mutate(.BINTIME_INDEX = as.numeric(cut(.data$TAFD, breaks, labels))) |>
+    mutate(BIN_LEFT = bin_par[.data$.BINTIME_INDEX, "left"]) |>
+    mutate(BIN_RIGHT = bin_par[.data$.BINTIME_INDEX, "right"]) |>
+    mutate(BINTIME = round(bin_par[.data$.BINTIME_INDEX, "label"])) |>
+    select(-c(".BINTIME_INDEX"))
+}
+
+
+#' BINTIME plot
+#'
+#' @param obj A nif object.
+#' @param analyte The analyte as character.
+#' @param method Univariate class intervals method, can be one of jenks, kmeans,
+#' pretty, quantile, hclust, sd, bclust or fisher. See classInt::classInterval()
+#' for details.
+#' @param points Plot original data points as logical.
+#' @param caption Show caption as logical.
+#' @param size The point size.
+#' @param alpha The alpha parameter for the data points.
+#'
+#' @returns A ggplot2 object.
+#' @export
+bintime_plot <- function(
+    obj,
+    analyte,
+    method = "fisher",
+    points = FALSE,
+    caption = TRUE,
+    size = 1.5,
+    alpha = 1) {
+  # input validation
+  validate_nif(obj)
+  validate_char_param(analyte, "analyte")
+  validate_analyte(obj, analyte)
+  validate_nif(obj)
+  validate_char_param(method, "method")
+  if(!method %in%c("jenks", "kmeans", "pretty", "quantile", "hclust", "sd",
+                   "bclust", "fisher")) {
+    stop(paste0("Method ", method, " not implemented!"))
+  }
+  # validate_numeric_param(breaks, allow_multiple = TRUE)
+  validate_logical_param(points)
+  validate_logical_param(caption, allow_null = TRUE)
+  validate_numeric_param(alpha)
+
+  if (is.null(caption))
+    caption <- FALSE
+
+  temp <- obj |>
+    filter(.data$ANALYTE == analyte) |>
+    filter(.data$EVID == 0) |>
+    add_bintime(method = method) |>
+    filter(!is.na(.data$BINTIME)) |>
+    filter(!is.na(.data$DV)) |>
+    as.data.frame()
+
+  out <- temp |>
+    reframe(
+      .data$BINTIME,
+      .data$ANALYTE,
+      .data$BIN_LEFT,
+      .data$BIN_RIGHT,
+      .individual_mean = mean(.data$DV),
+      .by = c("ID", "ANALYTE", "BINTIME")
+    ) |>
+    distinct() |>
+    reframe(
+      .data$BIN_LEFT,
+      .data$BIN_RIGHT,
+      mean = mean(.data$.individual_mean),
+      sd = sd(.data$.individual_mean),
+      n = n(),
+      se = sd/sqrt(.data$n),
+      df = .data$n - 1,
+      t = stats::qt(p = 0.05/2, df = .data$df, lower.tail = FALSE),
+      margin_error = .data$t * .data$se,
+      lower_ci = .data$mean - .data$margin_error,
+      upper_ci = .data$mean + .data$margin_error,
+      .by = c("BINTIME", "ANALYTE")) |>
+    distinct() |>
+
+    ggplot(aes(x = .data$BINTIME, y = .data$mean)) +
+    labs(y = analyte, x = "TAFD")
+
+  if (points == TRUE) {
+    out <- out +
+      geom_point(aes(x = .data$TAFD, y = .data$DV), data = temp, alpha = alpha,
+                 size = size)
+  }
+
+  out <- out +
+    geom_segment(aes(x = .data$BIN_LEFT, xend = .data$BIN_RIGHT,
+                     y = .data$mean), color = "red") +
+    geom_rect(aes(xmin = .data$BIN_LEFT, xmax = .data$BIN_RIGHT,
+                  ymin = .data$lower_ci, ymax = .data$upper_ci),
+              fill = "red", alpha = 0.3) +
+    theme_bw()
+
+  if (caption == TRUE) {
+    out <- out +
+      labs(caption = "Mean with 90% CI")
+  }
+
+  out
+}
+
+
+
 #' make QTIME field
 #'
 #' @param obj A nif object.
@@ -33,6 +179,10 @@ add_qtime <- function(obj, breaks) {
     )) |>
     left_join(rect, by = "QTIME")
 }
+
+
+
+
 
 
 #' Plot analyte over time by discretized time after first dose (QTIME)
@@ -92,22 +242,6 @@ qtime_plot <- function(
       .by = c("ID", "ANALYTE", "QTIME")
     ) |>
     distinct() |>
-
-    # reframe(
-    #   .data$.LEFT,
-    #   .data$.RIGHT,
-    #   mean = mean(.data$DV),
-    #   sd = sd(.data$DV),
-    #   n = n(),
-    #   se = sd/sqrt(.data$n),
-    #   df = .data$n - 1,
-    #   t = stats::qt(p = 0.05/2, df = .data$df, lower.tail = FALSE),
-    #   margin_error = .data$t * .data$se,
-    #   lower_ci = .data$mean - .data$margin_error,
-    #   upper_ci = .data$mean + .data$margin_error,
-    #   .by = c("QTIME", "ANALYTE")) |>
-    # distinct() |>
-
     reframe(
       .data$.LEFT,
       .data$.RIGHT,
